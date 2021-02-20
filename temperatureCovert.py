b1e536d49a0d287943814dcabf79b66f27e18a28
abokideveloper1 <xandykelvin@gmail.com>
2021-02-20 10:45:16 -0300

    Update:new code file has been added!

---
+++ temperatureCovert.py
@@ -0,0 +1,3 @@
+celsius = float(input("Enter celsius temperature: "))
+farenheit = (1.8 * celsius) + 32
+print("Following temperature {:.1f} celsius degrees equals {:.1f} farenheit degrees.".format(celsius,farenheit))
